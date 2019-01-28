890300768a8a9105c51a44065159cf17785ba706
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-01-28 19:24:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,5 +8,10 @@ def countdown(count)
 end
 
 def countdown_with_sleep(count)
-  sleep(1)
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
