de6efc1dee8784a2831ef7ceb5c870c38bf15184
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-01-28 19:32:11 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,17 @@
-#write your code here
 
-def countdown(integer)
-  while integer > 0
-    puts "#{integer} SECOND(S)!"
-    integer -= 1
+def countdown(count)
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
   end
-puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(count)
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
